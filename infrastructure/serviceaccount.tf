11ea637702cdf7db9598b4e315b58165717473dc
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-27 16:06:28 +0200

    fixed random length

--- infrastructure/serviceaccount.tf
+++ infrastructure/serviceaccount.tf
@@ -6,21 +6,21 @@ resource "google_service_account" "service_account" {
 }
 
 #Add Compute Admin Role to service account
-resource "google_service_account_iam_binding" "compute_admin" {
-  service_account_id = google_service_account.service_account.name
-  role               = google_project_iam_custom_role.custom-role.role_id
+#resource "google_service_account_iam_binding" "compute_admin" {
+#  service_account_id = google_service_account.service_account.name
+#  role               = google_project_iam_custom_role.custom-role.role_id
 
-  members = [
-    "serviceAccount:${google_service_account.service_account.email}",
-  ]
-}
+#  members = [
+#    "serviceAccount:${google_service_account.service_account.email}",
+#  ]
+#}
 
-resource "google_project_iam_custom_role" "custom-role" {
-  role_id     = "acmecmdbRole"
-  title       = "${var.academy_prefix}-${var.project_name}-sa-role"
-  description = "ACME CMDB Custom Role"
-  permissions = [
-    "roles/compute.instanceAdmin"
-    #"roles/compute."
-    ]
-}
+#resource "google_project_iam_custom_role" "custom-role" {
+#  role_id     = "acmecmdbRole"
+#  title       = "${var.academy_prefix}-${var.project_name}-sa-role"
+#  description = "ACME CMDB Custom Role"
+#  permissions = [
+#    "roles/compute.instanceAdmin"
+#    #"roles/compute."
+#    ]
+#}
