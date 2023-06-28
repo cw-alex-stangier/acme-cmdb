82d6e01d3bd68518f6a370068d2a98b18c821601
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-28 11:01:26 +0200

    fixed output

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,15 +1,15 @@
 output "urls" {
   value = {
     app  = google_cloud_run_service.service.status[0].url
-    registry = google_artifact_registry_repository.registry.url
+    registry = google_artifact_registry_repository.registry.id
   }
 }
 
-output "service-accounts" {
-  value = {
-    cicd = google_service_account.service_account_cicd.email
-    cicd-pub-key = google_service_account_key.service_account_cicd.public_key
-    cmdb = google_service_account.service_account_cmdb.email
-    cmdb-pub-key = google_service_account_key.service_account_cicd.public_key
-  }
-}
+#output "service-accounts" {
+#  value = {
+#    cicd = google_service_account.service_account_cicd.email
+#    cicd-pub-key = google_service_account_key.service_account_cicd.public_key
+#    cmdb = google_service_account.service_account_cmdb.email
+#    cmdb-pub-key = google_service_account_key.service_account_cicd.public_key
+#  }
+#}
