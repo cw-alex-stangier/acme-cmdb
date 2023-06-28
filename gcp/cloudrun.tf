b8927f96aba44386885368c90543083059864bdb
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-28 11:32:06 +0200

    mybe fixed cr

--- gcp/cloudrun.tf
+++ gcp/cloudrun.tf
@@ -1,6 +1,14 @@
 resource "google_cloud_run_service" "service" {
   name     = "${var.academy_prefix}-${var.project_name}-run"
   location = var.target_region
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
 }
 
 data "google_iam_policy" "admin" {
