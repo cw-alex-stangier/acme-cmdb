e747c2efac7e9e6cc55911a4825604cab8a89e22
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-29 13:25:26 +0200

    changed roles

--- gcp/serviceAccounts.tf
+++ gcp/serviceAccounts.tf
@@ -12,6 +12,7 @@ resource "google_service_account" "service_account_cmdb" {
   description   = "[AS] ACME CMDB Service Account"
 }
 
+#Assign basic role
 resource "google_service_account_iam_binding" "serviceAccountUserRole" { 
   service_account_id = google_service_account.service_account_cicd.name
 
@@ -21,6 +22,30 @@ resource "google_service_account_iam_binding" "serviceAccountUserRole" {
   members = [
     "serviceAccount:${google_service_account.service_account_cicd.email}",
     "serviceAccount:${google_service_account.service_account_cmdb.email}",
+  ]
+}
+
+#Assign CICD specific roles
+resource "google_service_account_iam_binding" "serviceAccountCICDRole" { 
+  service_account_id = google_service_account.service_account_cicd.name
+
+  for_each   = toset(["roles/run.admin", "roles/artifactregistry.admin"])
+  role       = each.key
+
+  members = [
+    "serviceAccount:${google_service_account.service_account_cicd.email}",
+  ]
+}
+
+#Assign CMDB specific roles
+resource "google_service_account_iam_binding" "serviceAccountCICDRole" { 
+  service_account_id = google_service_account.service_account_cicd.name
+
+  for_each   = toset(["roles/compute.instanceAdmin.v1"])
+  role       = each.key
+
+  members = [
+    "serviceAccount:${google_service_account.service_account_cicd.email}",
   ]
 }
 
