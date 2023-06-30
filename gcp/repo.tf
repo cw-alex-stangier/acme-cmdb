d543ed6df1a376838734ed18fe9f3ac5eb8a49f0
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-30 11:59:07 +0200

    auto add repo as remote

--- gcp/repo.tf
+++ gcp/repo.tf
@@ -1,3 +1,8 @@
 resource "google_sourcerepo_repository" "repo" {
-  name = "acme-cmdb"
+  name = var.project_name
+
+  provisioner "local-exec" {
+      command = "git remote add acme-repo https://source.developers.google.com/p/${var.project}/r/${var.project_name}"
+      interpreter = ["sh"]
+  }
 }
