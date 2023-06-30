319c41dff6bdb341293fb0ae17baa144203abd98
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-30 11:26:47 +0200

    check in keygen

--- keygen.sh
+++ keygen.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+gcloud init
+
 gcloud iam service-accounts keys create cicd_key.json --iam-account=as-acme-cmdb-1@cw-academy-sandbox-alex.iam.gserviceaccount.com
 
 gcloud iam service-accounts keys create cmdb_key.json --iam-account=as-acme-cmdb-2@cw-academy-sandbox-alex.iam.gserviceaccount.com
 
-mkdir keys
+[ ! -d "/keys/" ] && mkdir keys
 
 mv cicd_key.json ./keys/cicd_key.json
 
