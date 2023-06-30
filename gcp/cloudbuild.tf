4ee6469c57021c38aa3ea935ae0477cc9b336f8a
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-06-30 13:00:21 +0200

    added cloud build trigger and cloudbuild file

---
+++ gcp/cloudbuild.tf
@@ -0,0 +1,18 @@
+module "cloudbuild_github" {
+  source  = "memes/cloudbuild/google//modules/github"
+  version = "1.1.0"
+
+  name = "${var.env}-${var.academy_prefix}-${var.project_name}-trigger"
+  project_id = var.project_id
+  source_repo = "cw-alex-stangier/acme-cmdb"
+
+  trigger_config = {
+    branch_regex = "main$"
+    is_pr_trigger = true
+    comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+    tag_regex = null
+  }
+
+  filename = "../cloudbuild.yml"
+
+}
--- gcp/repo.tf
+++ gcp/repo.tf
@@ -2,15 +2,19 @@ resource "google_sourcerepo_repository" "repo" {
   name = "${var.academy_prefix}-${var.env}-${var.project_name}-repo"
   project = var.project
 
+  #add current repo
   provisioner "local-exec" {
-      command = "git remote remove ${google_sourcerepo_repository.repo.name}"
+    command = "git remote add ${google_sourcerepo_repository.repo.name} https://source.developers.google.com/p/${var.project}/r/${google_sourcerepo_repository.repo.name}"
   }
 
+  #push to repo
   provisioner "local-exec" {
-      command = "git remote add ${google_sourcerepo_repository.repo.name} https://source.developers.google.com/p/${var.project}/r/${google_sourcerepo_repository.repo.name}"
+    command = "git push ${google_sourcerepo_repository.repo.name} main"
   }
 
+  #remove remote repo
   provisioner "local-exec" {
-      command = "git push ${google_sourcerepo_repository.repo.name} main"
+    when    = destroy
+    command = "git remote remove ${google_sourcerepo_repository.repo.name}"
   }
 }
