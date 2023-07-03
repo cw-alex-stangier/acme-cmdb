9c32ae81c6a62c141c9188d45a20201fb3afb28d
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-07-03 12:59:31 +0200

    sa secret managment

--- gcp/repo.tf
+++ gcp/repo.tf
@@ -2,11 +2,6 @@ resource "google_sourcerepo_repository" "repo" {
   name = "${var.academy_prefix}-${var.env}-${var.project_name}-repo"
   project = var.project
 
-  #remove remote repo
-  provisioner "local-exec" {
-    command = "git remote remove ${self.name}"
-  }
-
   #add current repo
   provisioner "local-exec" {
     command = "git remote add ${google_sourcerepo_repository.repo.name} https://source.developers.google.com/p/${var.project}/r/${google_sourcerepo_repository.repo.name}"
