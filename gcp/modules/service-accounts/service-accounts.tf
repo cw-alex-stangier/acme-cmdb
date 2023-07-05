1b38d262e22080bbf61bfa607bf3db7dd5f2b076
Alex Stangier <alex.stangier@cloudwuerdig.com>
2023-07-05 11:22:46 +0200

    add act as permission to sa acc 10

--- gcp/modules/service-accounts/service-accounts.tf
+++ gcp/modules/service-accounts/service-accounts.tf
@@ -72,6 +72,14 @@ resource "google_project_iam_member" "serviceAccountCMDBRole" {
   member = "serviceAccount:${google_service_account.service_account_cmdb.email}"
 }
 
+resource "google_project_iam_member" "serviceAccountCloudRun" { 
+  project = var.project
+  for_each   = toset(["roles/run.admin", "roles/cloudbuild.serviceAgent"])
+  role       = each.key
+
+  member = "serviceAccount:${var.project_number}-compute@developer.gserviceaccount.com"
+}
+
 resource "google_project_iam_binding" "add-serviceAccountUser-Role" {
   project =  var.project
   role = "roles/iam.serviceAccountUser"
